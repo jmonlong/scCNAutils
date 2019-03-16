a9e9a32641ae08004a4cc7efb04e7def2da11461
Jean Monlong <jmonlong@ucsc.edu>
2019-03-15 22:18:08 -0700

    annotate CNAs with genes

--- tests/testthat/test_cna.R
+++ tests/testthat/test_cna.R
@@ -16,6 +16,28 @@ df = cbind(df, mat)
 ## Metacell info
 mc.info = data.frame(cell=cells, community=factor(rep(1:2,5)), stringsAsFactors=FALSE)
 
+## Genes
+genes.df = data.frame(geneid=1:nb.genes, symbol=paste0('gene', 1:nb.genes),
+                      stringsAsFactors=FALSE)
+## Add one duplicate
+genes.df[1,] = genes.df[2,]
+## Coordinates
+genes.coord = data.frame(symbol=genes.df$symbol,
+                         chr=sample(1:2, nb.genes, TRUE),
+                         start=sample.int(10000, nb.genes),
+                         stringsAsFactors=FALSE)
+genes.coord$end = genes.coord$start + 10
+
+## Gene expression
+mat = matrix(rpois(nb.cells*nb.genes, 1), nb.genes, nb.cells)
+cells = paste0('barcode', 1:nb.cells)
+colnames(mat) = cells
+ge = data.frame(symbol=paste0('gene', 1:nb.genes), stringsAsFactors=FALSE)
+ge = cbind(ge, mat)
+
+
+
+
 test_that("CNAs are called without errors", {
   cna.o = call_cna(df, mc_info=mc.info)
   expect_gt(nrow(cna.o$seg.df), 0)  
@@ -36,4 +58,16 @@ test_that("CNAs are called without errors with multisamp", {
   tmp = lapply(ggp, print)
   dev.off()
   expect_true(file.remove('temp.pdf'))  
+})
+
+test_that("Get gene info and annotate CNAs", {
+  gene.info = gene_info(ge, genes.coord)
+  expect_true(any(!is.na(gene.info$exp.mean)))
+  expect_true(any(!is.na(gene.info$exp.sd)))
+  expect_true(any(!is.na(gene.info$prop.non0)))
+  cna.o = call_cna_multisamps(df, mc.info)
+  seg.df = annotate_cna(cna.o$seg.df, gene.info)
+  expect_gt(nrow(seg.df), 0)
+  expect_true(any(!is.na(seg.df$exp.genes)))
+  expect_true(any(!is.na(seg.df$all.genes)))
 })
