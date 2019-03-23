17a5058bc8d343bbfd120a1383a6dcb67d434a82
Jean Monlong <jean.monlong@mail.mcgill.ca>
2019-03-23 01:22:31 -0400

    debug annotation and heatmap

--- R/gene_info.R
+++ R/gene_info.R
@@ -10,21 +10,36 @@
 ##' It's better to run this function after normalization (\code{norm_ge}
 ##' but before binning (\code{bin_genes}).
 ##' @title Gene information
-##' @param ge_df a data.frame or path to a file with gene expression
+##' @param ge_df a data.frame with gene expression
 ##' information across cells. E.g. after \code{norm.ge}.
 ##' @param genes_coord either a file name or a data.frame with coordinates
 ##' and gene names.
+##' @param subset_cells the maximum number of cells to use.
 ##' @return a data.frame with summary stats for each gene.
 ##' @author Jean Monlong
 ##' @export
-gene_info <- function(ge_df, genes_coord){
+gene_info <- function(ge_df, genes_coord, subset_cells=1e4){
   if(!all(c('chr','start','end') %in% colnames(ge_df)) &
      !('symbol' %in% colnames(ge_df))){
     stop('"ge_df" must have either a column "symbol", or coordinates columns chr/start/end.')
   }
 
+  if(!is.data.frame(genes_coord)){
+    if(is.character(genes_coord) & length(genes_coord) == 1){
+      genes_coord = utils::read.table(genes_coord, as.is=TRUE, header=TRUE)
+    } else {
+      stop('genes_coord must be either a file name or a data.frame.')
+    }
+  }
+
   ge.info = ge_df[, intersect(colnames(ge_df), c('symbol', 'chr', 'start', 'end'))]
   cells = setdiff(colnames(ge_df), c('symbol', 'chr', 'start', 'end'))
+
+  ## Subset cells
+  if(!is.null(subset_cells) && length(cells)>subset_cells){
+    cells = sample(cells, subset_cells)
+  }
+  
   ## Compute gene-level metrics
   ge.info$exp.mean = apply(as.matrix(ge_df[, cells]), 1, mean, na.rm=TRUE)
   ge.info$exp.sd = apply(as.matrix(ge_df[, cells]), 1, stats::sd, na.rm=TRUE)
--- R/plot_heatmap.R
+++ R/plot_heatmap.R
@@ -3,11 +3,13 @@
 ##' @param cells_df a data.frame with cell information.
 ##' @param nb_subsamp the number of cells to sub-sample. Default is 1000.
 ##' @param hc.method the hierarchical clustering method to order cells.
+##' @param z_win threshold to winsorized Z scores in the color scale.
 ##' @return a list of ggplot2 objects.
 ##' @author Jean Monlong
 ##' @external
 ##' @import ggplot2
-plot_heatmap <- function(z_df, cells_df=NULL, nb_subsamp=1000, hc.method='ward.D'){
+plot_heatmap <- function(z_df, cells_df=NULL, nb_subsamp=1000,
+                         hc.method='ward.D', z_win=3){
   chr = start = end = cell = cello = z = NULL
   cells = setdiff(colnames(z_df), c('chr', 'start', 'end'))
 
@@ -21,13 +23,13 @@ plot_heatmap <- function(z_df, cells_df=NULL, nb_subsamp=1000, hc.method='ward.D
 
   ## Melt score matrix
   z_df = tidyr::gather(z_df, cell, z, -chr, -start, -end)
-  z_df$cell = factor(z_df$cell, levels=hc.o$labels[hc.o$order])
   
   ## Merge cells info
   cells_df = cells_df[, intersect(colnames(cells_df), c('cell', 'community', 'sample'))]
-  z_df = merge(z_df, cells_df)
+  z_df = merge(z_df, cells_df, all.x=TRUE)
 
-  ## Order for chromosomes
+  ## Order for cells and chromosomes
+  z_df$cell = factor(as.character(z_df$cell), levels=hc.o$labels[hc.o$order])
   if(all(grepl('chr', z_df$chr))){
     z_df$chr = factor(z_df$chr, levels=paste0('chr', c(1:22, 'X','Y')))
   } else {
@@ -36,35 +38,46 @@ plot_heatmap <- function(z_df, cells_df=NULL, nb_subsamp=1000, hc.method='ward.D
 
   ## ggplot2
   ggp.l = list()
-  ggp.l$all = ggplot(z_df, aes(x=start, xend=end, y=as.numeric(cell),
-                               yend=as.numeric(cell)+1)) +
+  ggp.l$all = ggplot(z_df, aes(xmin=start, xmax=end, ymin=as.numeric(cell)-.5,
+                               ymax=as.numeric(cell)+.5,
+                               fill=winsor(z, z_win, -z_win))) +
     geom_rect() + theme_bw() +
-    facet_grid(.~chr, scales='free') +
-    ylab('cell') + xlab('position') + 
-    theme(axis.text.y=element_blank(), axis.text.x=element_blank())
+    facet_grid(.~chr, scales='free', space='free') +
+    ylab('cell') + xlab('position') +
+    scale_fill_gradient2(name="z", low=scales::muted("blue"),
+                         high=scales::muted("red")) + 
+    theme(axis.text.y=element_blank(), axis.text.x=element_blank(), legend.position='bottom')
 
   ## Group by communities
   if('community' %in% colnames(z_df)){
     cell.order = unique(z_df$cell[order(z_df$community, z_df$cell)])
     z_df$cello = factor(z_df$cell, levels=cell.order)
-    ggp.l$community = ggplot(z_df, aes(x=start, xend=end, y=as.numeric(cello),
-                                       yend=as.numeric(cello)+1)) +
+    ggp.l$community = ggplot(z_df, aes(xmin=start, xmax=end,
+                                       ymin=as.numeric(cello)-.5,
+                                       ymax=as.numeric(cello)+.5,
+                                       fill=winsor(z, z_win, -z_win))) +
     geom_rect() + theme_bw() +
-    facet_grid(community~chr, scales='free') +
+    facet_grid(community~chr, scales='free', space='free') +
     ylab('cell') + xlab('position') + 
-    theme(axis.text.y=element_blank(), axis.text.x=element_blank())
+    scale_fill_gradient2(name="z", low=scales::muted("blue"),
+                         high=scales::muted("red")) + 
+    theme(axis.text.y=element_blank(), axis.text.x=element_blank(), legend.position='bottom')
   }
 
   ## Group by sample
   if('sample' %in% colnames(z_df)){
     cell.order = unique(z_df$cell[order(z_df$sample, z_df$cell)])
     z_df$cello = factor(z_df$cell, levels=cell.order)
-    ggp.l$community = ggplot(z_df, aes(x=start, xend=end, y=as.numeric(cello),
-                                       yend=as.numeric(cello)+1)) +
+    ggp.l$sample = ggplot(z_df, aes(xmin=start, xmax=end,
+                                       ymin=as.numeric(cello)-.5,
+                                       ymax=as.numeric(cello)+.5,
+                                       fill=winsor(z, z_win, -z_win))) +
     geom_rect() + theme_bw() +
-    facet_grid(sample~chr, scales='free') +
+    facet_grid(sample~chr, scales='free', space='free') +
     ylab('cell') + xlab('position') + 
-    theme(axis.text.y=element_blank(), axis.text.x=element_blank())
+    scale_fill_gradient2(name="z", low=scales::muted("blue"),
+                         high=scales::muted("red")) + 
+    theme(axis.text.y=element_blank(), axis.text.x=element_blank(), legend.position='bottom')
   }
 
   return(ggp.l)
